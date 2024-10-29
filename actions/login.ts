b152d6a2c1e1c4dc06bc06ef9106d5ff0468c52d
priyam bhardwaj <priyam3801h@gmail.com>
2024-10-29 23:25:39 +0530

    i

--- actions/login.ts
+++ actions/login.ts
@@ -1,35 +1,32 @@
+import { LoginSchema } from "@/schema";
+import * as z from "zod";
+import useCurrentUser from "@/store";
 
+export const Login = async (values: z.infer<typeof LoginSchema>) => {
+  if (!values.email || !values.password) {
+    return { error: "kindly fill all the fields" };
+  }
 
-import { LoginSchema } from "@/schema"
-import * as z from "zod"
-import useCurrentUser from "@/store"
-
-
-export const Login =  (async(values : z.infer<typeof LoginSchema>)=>{
-
-    
-    if(!values.email || !values.password){
-        return {error : "kindly fill all the fields"}
+  const res = await fetch(
+    "https://vercel.com/b-priyams-projects/chat-app-backend-lqvt/users/getUser",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: values.email,
+        password: values.password,
+      }),
     }
-    
-    const res = await fetch('http://localhost:3000/users/getUser',{
-        method : "POST",
-        headers : {"Content-Type" : "application/json"},
-        body : JSON.stringify({
-            email : values.email,
-            password : values.password
-        })
-    })
-    
-    const response = await res.json();
-    console.log(response)
-    if(response.error){
-        return {error : response.error}
-    }
-    
-    if(response.id){
-        return {success : "Login successful", data : response}
-    }
-    return {error : "Something went wrong"}
+  );
 
-})
+  const response = await res.json();
+  console.log(response);
+  if (response.error) {
+    return { error: response.error };
+  }
+
+  if (response.id) {
+    return { success: "Login successful", data: response };
+  }
+  return { error: "Something went wrong" };
+};
